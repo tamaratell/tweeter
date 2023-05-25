963318d6838261406a6728984960ef5ad0a04d1a
Tamara Tell <2telltamara@gmail.com>
2023-05-25 15:38:54 -0400

    implement createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//CREATE TWEET ELEMENT TO GO IN TWEETS ON PAGE: 
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+};
+
+const createTweetElement = function(tweet) {
+  tweet = tweet[0];
+  let $tweet = `
+  <article class="tweet-container">
+      <header>
+        <div>
+          <img class="tweet-user-icon" src="${tweet.user.avatars}">
+          <p>${tweet.user.name}</p>
+        </div>
+        <p class="user-handle">${tweet.user.handle}</p>
+      </header>
+      <div class="tweet-content">
+        <p> ${tweet.content.text} </p>
+      </div>
+      <footer>
+        <p> ${tweet.created_at} </p>
+        <p class="icons"> <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </p>
+      </footer>
+    </article>`;
+  return $tweet;
+};
+
+renderTweets(data);
+
+
