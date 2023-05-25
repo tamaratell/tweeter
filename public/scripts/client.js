05745207efa94ae1e0c896859f3769d2c4fd382e
Tamara Tell <2telltamara@gmail.com>
2023-05-25 17:35:43 -0400

    renderTweets succesfully implemented in client.js. tweeter is rendering hard coded tweet data as new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//CREATE TWEET ELEMENT TO GO IN TWEETS ON PAGE: 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+$(() => {
+  //CREATE TWEET ELEMENT TO GO IN TWEETS ON PAGE: 
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-};
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (let tweet in tweets) {
+      // calls createTweetElement for each tweet
+      const formattedTweet = createTweetElement(tweets[tweet]);
+      // takes return value and appends it to the tweets container
+      $('.tweet-container').append(formattedTweet);
+    }
+    return;
+  };
 
-const createTweetElement = function(tweet) {
-  tweet = tweet[0];
-  let $tweet = `
-  <article class="tweet-container">
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+  <article class="tweet">
       <header>
         <div>
           <img class="tweet-user-icon" src="${tweet.user.avatars}">
@@ -60,9 +65,10 @@ const createTweetElement = function(tweet) {
         </p>
       </footer>
     </article>`;
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+  renderTweets(data);
 
+});
 
