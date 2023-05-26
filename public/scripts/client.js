b027ee90121c9981f7c4785d3fdb92668948796b
Tamara Tell <2telltamara@gmail.com>
2023-05-26 02:10:09 -0400

    implement jQuery to add/hide modal instead of using alert. if you are planning on going back to this version, remember that implementing this modal function took you 2 hours the first time and you almost threw your laptop out the window so DONT TOUCH ANYTHING

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,6 @@
  */
 
 $(() => {
-  let tweetsLoaded = false;
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -50,7 +49,6 @@ $(() => {
   };
 
 
-
   const loadTweets = () => {
     $.get('/tweets', function(response) {
       renderTweets(response);
@@ -65,29 +63,75 @@ $(() => {
     const $form = $(this);
     const tweetData = $form.find('#tweet-text').val().trim();
 
-    if (tweetData.length > 140 || tweetData.length === 0) {
-      alert("Tweet must be between 1 and 140 characters");
-      return;
-    }
+    const createModal = (tweetLength) => {
+      let $modal = $(`<div class="modal">
+        <div class="modal-content">
+          <button class="close-button"> X </button>
+          <p><i class="fa-solid fa-triangle-exclamation"></i></p>
+        </div>`);
 
-    $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data: $form.serialize(),
-      success: function(response) {
-        console.log("Success");
-        loadTweets();
-      },
-      error: function(xhr, status, error) {
-        // Handle the error response
-        console.log('POST request failed');
-        console.log(error);
+      let $para = $modal.find('p');
+
+      if (tweetLength > 140) {
+        $para.append("Woah there! Your tweet is on the verge of starting a novel. Keep it to 140 characters or less.");
       }
+
+      if (tweetLength === 0) {
+        $para.append("Uh-oh! It seems your tweet is feeling shy and speechless. Please enter some words and hit that tweet button!");
+      }
+
+      return $modal;
+    };
+
+    const showModal = (tweetLength) => {
+      const $modal = createModal(tweetLength);
+      $('main').prepend($modal);
+      return $modal;
+    };
+
+    const validateTweet = (tweetData) => {
+      if (tweetData.length > 140 || tweetData.length === 0) {
+        return false;
+      }
+      return true;
+    };
+
+    $('#new-tweet').submit(function(event) {
+      event.preventDefault();
+      const $form = $(this);
+      const tweetData = $form.find('#tweet-text').val().trim();
+
+      const $existingModal = $('main').find('.modal');
+      if ($existingModal.length) {
+        $existingModal.remove();
+        return;
+      }
+
+      if (!validateTweet(tweetData)) {
+        const $modal = showModal(tweetData.length);
+        $modal.on('click', '.close-button', () => {
+          $modal.slideUp();
+        });
+        return;
+      }
+
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $form.serialize(),
+        success: function(response) {
+          console.log("Success");
+          loadTweets();
+        },
+        error: function(xhr, status, error) {
+          // Handle the error response
+          console.log('POST request failed');
+          console.log(error);
+        }
+      });
+
+      $('#tweet-text').val("");
+      $('.counter').val(140);
     });
-
-    $('#tweet-text').val("");
-    $('.counter').val(140);
-
   });
 });
-
